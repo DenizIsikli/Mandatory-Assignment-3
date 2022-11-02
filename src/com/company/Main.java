24b9b798dc51909266b80af9375f5dad4b2146f2
DenizIsikli <s215818@dtu.dk>
2022-11-02 21:40:21 +0100

    Queue class

---
+++ src/com/company/Main.java
@@ -0,0 +1,78 @@
+package com.company;
+
+import java.util.concurrent.Semaphore;
+
+public class Main {
+
+    public static void main(String[] args) {
+        QueueThread queueThread = new QueueThread();
+        int customersWaitingInLine = 5;
+        int customersPaying = 2;
+
+        Thread box1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int customerCount = 0;
+                while (customerCount < customersPaying) {
+                    Customer customer = new Customer(queueThread);
+                    Thread thread1 = new Thread(customer);
+                    customer.setName("" + thread1.getId());
+                    thread1.start();
+                    customerCount++;
+                    System.out.println("In thread: " + Thread.currentThread().getName());
+
+                    try {
+                        Thread.sleep((long) (Math.random()*(10-1)+1));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        Thread box2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int customerCount = 0;
+                while (customerCount < customersPaying) {
+                    Customer customer = new Customer(queueThread);
+                    Thread thread1 = new Thread(customer);
+                    customer.setName("" + thread1.getId());
+                    thread1.start();
+                    customerCount++;
+                    System.out.println("In thread: " + Thread.currentThread().getName());
+
+                    try {
+                        Thread.sleep((long) (Math.random()*(10-1)+1));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        Thread inLine = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int customerCount = 0;
+                while (customersWaitingInLine > customerCount) {
+                    Customer customer = new Customer(queueThread);
+                    Thread thread1 = new Thread(customer);
+                    customer.setName("" + thread1.getId());
+                    thread1.start();
+                    customerCount++;
+
+                    try {
+                        Thread.sleep((long) (Math.random()*(10-1)+1));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        box1.start();
+        box2.start();
+        inLine.start();
+    }
+}
